84f2fb4e08e69aca90e5950deb22cc9fbb231254
chplushsieh <chplushsieh@gmail.com>
2016-04-08 20:48:20 -0500

    environment mapping done

---
+++ learn_opengl_cubemaps/mp1/camera.h
@@ -0,0 +1,13 @@
+//
+//  camera.h
+//  mp1
+//
+//  Created by Chia-Hao Hsieh on 4/8/16.
+//  Copyright © 2016 Chia-Hao Hsieh. All rights reserved.
+//
+
+#ifndef camera_h
+#define camera_h
+
+
+#endif /* camera_h */
